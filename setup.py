515a4054d6d1a4c9f54aab6f62565be86ed303c3
NikilPrasad <105350616+NikilPrasad@users.noreply.github.com>
2023-11-13 22:22:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setuptools.setup(
     include_package_data=True,
     name='math_quiz',
     version='0.0.1',
-    description='Nikil99 python module',
+    description='NikilPrasad python module',
     url='https://github.com/NikilPrasad/nikildsshw.git',
     author='NikilPrasad',
-    author_email='contact@nikil99.com',
+    author_email='contact@NikilPrasad.com',
     packages=setuptools.find_packages(),
     install_requires=['pandas', 'pytest'
     ],
-    long_description='nikil99 python module',
+    long_description='NikilPrasad python module',
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
