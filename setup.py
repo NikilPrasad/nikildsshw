d5c2d1319cc4c6bd1564d0bfddfdd4ddece253ca
NikilPrasad <105350616+NikilPrasad@users.noreply.github.com>
2023-11-13 19:37:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-
+setup(
+    name='dsshw',
+    version='0.1.0',
+    packages=find_packages(include=['dsshw', 'dsshw.*']),
+    install_requires=[
+        'PyYAML',
+        'pandas==0.23.3',
+        'numpy>=1.14.5',
+        'matplotlib>=2.2.0,,
+        'jupyter'
+    ]
+)
